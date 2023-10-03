2a64938977e38d4df975c7f42e16e427d9bf9ace
Ömer Adan <omeradaan@gmail.com>
2023-10-03 23:56:20 +0300

    campaigns update

--- components/Campaigns.jsx
+++ components/Campaigns.jsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 
 const CampaingItem = () => {
   return (
-  <div className="bg-white">
-    <div className="relative w-40 h-40 after:content-[''] after:absolute after:w-full after:h-full after:border-[5px] after:border-secondary after:rounded-full">
+  <div className="bg-primary">
+    <div className="relative w-44 h-44 after:content-[''] after:absolute after:w-full after:h-full after:border-[5px] after:border-secondary after:rounded-full rounded-full overflow-hidden">
       <Image
       src="/images/o1.jpg"
       alt=""
       layout="fill"
+      objectFit="cover"
       />
     </div>
     </div>
@@ -19,7 +20,7 @@ const CampaingItem = () => {
 
 const Campaigns = () => {
   return (
-    <div className="flex container mx-auto py-20 my-20 ">
+    <div className="flex justify-between container mx-auto py-20 my-20 border-box">
         <CampaingItem/>
         <CampaingItem/>
 
