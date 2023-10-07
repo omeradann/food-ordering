3f2f796b9ab6ce7f0c7ab5a0fb256285a23cb782
Ömer Adan <omeradaan@gmail.com>
2023-10-07 17:26:48 +0300

    campaigns size update

--- components/Campaigns.jsx
+++ components/Campaigns.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const CampaingItem = () => {
   return (
-  <div className="bg-primary">
+  <div className="bg-primary flex-1 rounded-md py-5 px-5">
     <div className="relative w-44 h-44 after:content-[''] after:absolute after:w-full after:h-full after:border-[5px] after:border-secondary after:rounded-full rounded-full overflow-hidden">
       <Image
       src="/images/o1.jpg"
@@ -20,7 +20,7 @@ const CampaingItem = () => {
 
 const Campaigns = () => {
   return (
-    <div className="flex justify-between container mx-auto py-20 my-20 border-box">
+    <div className="flex justify-between container mx-auto py-20 my-20 gap-7">
         <CampaingItem/>
         <CampaingItem/>
 
