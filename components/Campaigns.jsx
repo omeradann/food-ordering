83f99be71f0287cd7c9ce41564c2795841800f9b
Ömer Adan <omeradaan@gmail.com>
2023-10-09 20:21:22 +0300

    campaigns update

--- components/Campaigns.jsx
+++ components/Campaigns.jsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
+import Title from "./ui/Title";
 
 
 const CampaingItem = () => {
   return (
-  <div className="bg-primary flex-1 rounded-md py-5 px-5">
-    <div className="relative w-44 h-44 after:content-[''] after:absolute after:w-full after:h-full after:border-[5px] after:border-secondary after:rounded-full rounded-full overflow-hidden">
+  <div className="bg-primary flex-1 rounded-md py-5 px-5 flex items-center gap-x-4">
+    <div className="relative w-44 h-44  after:border-[10px] after:border-secondary rounded-full overflow-hidden">
       <Image
       src="/images/o1.jpg"
+      className="hover:scale-105 transition-all"
       alt=""
       layout="fill"
       objectFit="cover"
+    
+      
       />
+    </div>
+    <div className="text-white">
+    <Title addClass="text-2xl">Tasty Thursdays</Title>
+    <div>
+      <span className="text-4xl font-dancing font-bold">20%</span>
+      <span className="font-dancing m-1">Off</span>
+    </div>
+    
     </div>
     </div>
   
