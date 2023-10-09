621a20b982c858237376d91e4610d1edb159d898
Ömer Adan <omeradaan@gmail.com>
2023-10-09 22:57:05 +0300

    wrapper update

---
+++ components/product/MenuItem.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { FaShoppingCart } from "react-icons/fa";
+
+const MenuItem = () => {
+  return (
+    <div className='mt-10 bg-primary rounded-3xl'>
+        <div className="w-full bg-[#f1f2f3] h-[210px] rounded-bl-[46px] grid place-content-center">
+            <div className="relative w-40 h-40 rounded-md">
+        <Image
+        className="hover:scale-110 transition-all"
+        src="/images/f1.png"
+        alt=""
+        layout="fill"
+        />
+        </div>
+        
+        </div>
+        <div className="p-6  text-white">
+            <h4 className="text-xl font-semibold">Delicious Pizza</h4>
+            <p className="mt-2 text-[15px]">Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque</p>
+            <div className="flex justify-between mt-4">
+            <span className="mt-2">$20</span>
+            <button className="btn-primary !rounded-full !w-10 !h-10 !p-0 grid place-content-center"><FaShoppingCart size={20}/></button>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default MenuItem
--- components/product/MenuWrapper.jsx
+++ components/product/MenuWrapper.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import Title from '../ui/Title'
+import MenuItem from './MenuItem'
 
 function MenuWrapper() {
   return (
-    <div className='container mx-auto flex flex-col items-center mb-20'>
+    <div className='container mx-auto mb-20'>
+        <div className='flex flex-col items-center '>
         <Title addClass="text-4xl">Our Menu</Title>
         <div className='flex gap-x-10 mt-10'>
             <button className='px-6 py-2 bg-primary rounded-3xl text-white'>All</button>
             <button className='px-6 py-2 rounded-3xl'>Burger</button>
             <button className='px-6 py-2 rounded-3xl'>Pizza</button>
             <button className='px-6 py-2 rounded-3xl'>Drinks</button>
-           
         </div>
+        </div>
+        
+        <div className='mt-8 grid grid-cols-3 gap-4'>
+            <MenuItem/>
+            <MenuItem/>
+            <MenuItem/>
+        </div>
+        
     </div>
   )
 }
