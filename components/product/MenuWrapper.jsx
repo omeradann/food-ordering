57efdce04afcef383fdac2bb25ab9078bae7bfcf
Ömer Adan <omeradaan@gmail.com>
2023-10-09 21:13:05 +0300

    wrapper update

--- components/product/MenuWrapper.jsx
+++ components/product/MenuWrapper.jsx
@@ -3,13 +3,14 @@ import Title from '../ui/Title'
 
 function MenuWrapper() {
   return (
-    <div className='container mx-auto flex flex-col items-center'>
+    <div className='container mx-auto flex flex-col items-center mb-20'>
         <Title addClass="text-4xl">Our Menu</Title>
-        <div>
-            <button>All</button>
-            <button>Burger</button>
-            <button>Pizza</button>
-            <button>Other</button>
+        <div className='flex gap-x-10 mt-10'>
+            <button className='px-6 py-2 bg-primary rounded-3xl text-white'>All</button>
+            <button className='px-6 py-2 rounded-3xl'>Burger</button>
+            <button className='px-6 py-2 rounded-3xl'>Pizza</button>
+            <button className='px-6 py-2 rounded-3xl'>Drinks</button>
+           
         </div>
     </div>
   )
