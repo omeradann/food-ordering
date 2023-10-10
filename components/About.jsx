0faa7b06a4af6899e2d469e655f571e809134445
Ömer Adan <omeradaan@gmail.com>
2023-10-10 17:08:19 +0300

    about update

--- components/About.jsx
+++ components/About.jsx
@@ -1,20 +1,19 @@
 import Image from "next/image";
-import React from "react";
 import Title from "./ui/Title";
 
 const About = () => {
   return (
-    <div className=" bg-primary p-24">
-      <div className="container mx-auto flex items-center mt-20 gap-20">
-        <div>
+    <div className=" bg-primary p-24 mb-20">
+      <div className="container mx-auto flex items-center gap-20 justify-center flex-wrap">
+        <div className="flex justify-center ">
           <div className="relative w-[445px] h-[600px]">
             <Image src="/images/about-img.png" alt="" layout="fill" />
           </div>
         </div>
 
-        <div className="text-white">
-          <Title addClass="text-[40px]">We Are Feane</Title>
-          <p>
+        <div className="text-white md:w-1/2">
+          <Title addClass="text-[40px]">We Are Adan</Title>
+          <p className="my-5">
             There are many variations of passages of Lorem Ipsum available, but
             the majority have suffered alteration in some form, by injected
             humour, or randomised words which don't look even slightly
